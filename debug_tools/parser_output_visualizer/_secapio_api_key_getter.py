6533830dbc39c17b68f5c9d36c19b0bb664daf8a
Elijas <4084885+Elijas@users.noreply.github.com>
2023-09-13 09:48:07 +0300

    fix(debug_tools): improve message

--- debug_tools/parser_output_visualizer/_secapio_api_key_getter.py
+++ debug_tools/parser_output_visualizer/_secapio_api_key_getter.py
@@ -21,13 +21,13 @@ class SecapioApiKeyGetter:
             container = empty.container()
 
             container.write(
-                "It seems you're trying to download a new document that isn't currently in our database. We're currently using *sec-api.io* to handle the removal of the title 10-Q page and to download 10-Q Section HTML files. In the future, we aim to download these HTML files directly from the SEC EDGAR. For now, you can get a free API key from [sec-api.io](https://sec-api.io) and input it below. Thanks for your understanding!"
+                "It seems you're trying to download a new document that isn't currently in our database. We're currently using *sec-api.io* to handle the removal of the title 10-Q page and to download 10-Q Section HTML files. In the future, we aim to download these HTML files directly from the SEC EDGAR. For now, you can get a free API key from [sec-api.io](https://sec-api.io) and input it below."
             )
             self.api_key = container.text_input(
                 type="password",
                 label="Enter your API key here:",
             )
-            msg = "**Note:** We suggest setting the `SECAPIO_API_KEY` environment variable, possibly in an `.env` file. This method allows you to utilize the API key in your code without the need for manual entry each time."
+            msg = "**Note:** We suggest setting the `SECAPIO_API_KEY` environment variable, possibly in an `.env` file. This method allows you to utilize the API key without the need for manual entry each time."
             container.info(msg)
             if not self.api_key:
                 st.stop()
