15aea355ef89ee1f834f08b1b0488834928083e0
Elijas <4084885+Elijas@users.noreply.github.com>
2023-10-22 14:53:37 +0300

    chore: remove unused imports

--- tests/unit/processing_steps/test_top_level_section_title_classifier.py
+++ tests/unit/processing_steps/test_top_level_section_title_classifier.py
@@ -11,14 +11,10 @@ from sec_parser.processing_steps.pre_top_level_section_pruner import (
 from sec_parser.processing_steps.top_level_section_title_classifier import (
     TopLevelSectionTitleClassifier,
 )
-from sec_parser.semantic_elements.composite_semantic_element import (
-    CompositeSemanticElement,
-)
 from sec_parser.semantic_elements.semantic_elements import TextElement, TitleElement
 from sec_parser.semantic_elements.table_element import TableElement
 from sec_parser.semantic_elements.top_level_section_title import TopLevelSectionTitle
 from tests.unit._utils import assert_elements
-from tests.unit.processing_steps._utils import parse_initial_semantic_elements
 
 
 def html_tag(tag_name: str, text: str = "Hello World") -> HtmlTag:
