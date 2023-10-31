526f596bcadcd8fa9167e6ce14df5c47cdb65c0b
Elijas <4084885+Elijas@users.noreply.github.com>
2023-10-31 11:33:18 +0200

    docs(SingleElementCheck): Add docstring

--- sec_parser/processing_steps/individual_semantic_element_extractor/single_element_checks/abstract_single_element_check.py
+++ sec_parser/processing_steps/individual_semantic_element_extractor/single_element_checks/abstract_single_element_check.py
@@ -12,4 +12,12 @@ if TYPE_CHECKING:
 class AbstractSingleElementCheck(ABC):
     @abstractmethod
     def contains_single_element(self, element: AbstractSemanticElement) -> bool | None:
+        """
+        Designed to work as series of subsequent checks.
+        - Returning None means that the check is inconclusive, and the next check should be performed.
+        - Returning True means that no further checks are necessary, and the HTML element will be
+        later be able to be converted into a semantic element without any splits.
+        - Returning False means that the HTML element will be split into multiple semantic elements
+        of type NotYetClassifiedElement.
+        """
         raise NotImplementedError  # pragma: no cover
