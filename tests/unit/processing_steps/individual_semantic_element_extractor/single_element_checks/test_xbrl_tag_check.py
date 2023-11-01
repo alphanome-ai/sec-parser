e4563b98b9f614a5d121bf13a81112c2a92519ef
Asapanna Rakesh <rakeshark22@gmail.com>
2023-11-01 14:03:09 +0530

    test: add unit test for xbrl_tag_check with ix:nonnumeric tag.

--- tests/unit/processing_steps/individual_semantic_element_extractor/single_element_checks/test_xbrl_tag_check.py
+++ tests/unit/processing_steps/individual_semantic_element_extractor/single_element_checks/test_xbrl_tag_check.py
@@ -6,7 +6,7 @@ from sec_parser.semantic_elements.abstract_semantic_element import AbstractSeman
 from sec_parser.processing_steps.individual_semantic_element_extractor.single_element_checks.xbrl_tag_check import XbrlTagCheck
 
 
-def test_contains_single_element():
+def test_contains_single_element_ix_tag():
     # Arrange
     element = AbstractSemanticElement(Mock())
     element.html_tag.name = "ix"
@@ -17,3 +17,19 @@ def test_contains_single_element():
 
     # Assert
     assert actual is False
+
+
+def test_contains_single_element_ix_numeric_tag():
+    # Arrange
+    element = AbstractSemanticElement(Mock())
+    element.html_tag.name = "some-random-tag-name"
+    element.html_tag.contains_tag.side_effect =  lambda tag: True if tag == "ix:nonnumeric" else False
+    check = XbrlTagCheck()
+
+    # Act
+    actual = check.contains_single_element(element)
+
+    # Assert
+    assert actual is False
+
+test_contains_single_element_ix_numeric_tag()
