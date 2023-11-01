7041f36b2ee0caf1fc94b9360aa71235b4cf5947
Asapanna Rakesh <rakeshark22@gmail.com>
2023-11-01 14:26:32 +0530

    test: add unit test for HighlightedTextElement's create_from_element

--- tests/unit/semantic_elements/test_highlighted_text_element.py
+++ tests/unit/semantic_elements/test_highlighted_text_element.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock
 import bs4
 import pytest
 
+from sec_parser.semantic_elements.abstract_semantic_element import AbstractSemanticElement
 from sec_parser.processing_engine.html_tag import HtmlTag
 from sec_parser.semantic_elements.highlighted_text_element import (
     HighlightedTextElement,
@@ -23,6 +24,18 @@ def test_highlighted_text_element_initialization():
         match="styles must be specified for HighlightedElement",
     ):
         HighlightedTextElement(mock_html_tag, style=None)
+
+
+def test_highlighted_text_element_from_element():
+    # Arrange
+    element = AbstractSemanticElement(Mock())
+
+    # Act & Assert
+    with pytest.raises(
+        SecParserValueError,
+        match="Style must be provided.",
+    ):
+        _ = HighlightedTextElement.create_from_element(element, style=None, log_origin=None)
 
 
 def test_to_dict():
