5b3bfb5f410ce525460c6b3079152ebd13cf5098
Elijas <4084885+Elijas@users.noreply.github.com>
2023-11-26 14:34:21 +0200

    test: fix test_contains_single_element_ix_numeric_tag

--- tests/unit/processing_steps/individual_semantic_element_extractor/single_element_checks/test_xbrl_tag_check.py
+++ tests/unit/processing_steps/individual_semantic_element_extractor/single_element_checks/test_xbrl_tag_check.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock
 import bs4
 import pytest
 
+from sec_parser.processing_engine.html_tag import HtmlTag
 from sec_parser.processing_steps.individual_semantic_element_extractor.single_element_checks.xbrl_tag_check import (
     XbrlTagCheck,
 )
@@ -24,14 +25,10 @@ def test_contains_single_element_ix_tag():
     assert actual is False
 
 
-@pytest.mark.xfail(reason="https://github.com/alphanome-ai/sec-parser/issues/50")
 def test_contains_single_element_ix_numeric_tag():
     # Arrange
     element = AbstractSemanticElement(Mock())
-    element.html_tag.name = "some-random-tag-name"
-    element.html_tag.contains_tag.side_effect = (
-        lambda tag: True if tag == "ix:nonnumeric" else False
-    )
+    element.html_tag.name = "ix:numeric"
     check = XbrlTagCheck()
 
     # Act
