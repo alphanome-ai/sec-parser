bf4e90ed28a9abef1e40cc6a8d8f735efa0cf21e
Elijas <4084885+Elijas@users.noreply.github.com>
2023-12-22 09:57:02 +0200

    docs(codespaces): add example.py

--- .devcontainer/example.py
+++ .devcontainer/example.py
@@ -11,4 +11,10 @@ html = dl.get_filing_html(ticker="AAPL", form="10-Q")
 # Now, we can parse the filing HTML into a list of semantic elements:
 elements: list = sp.Edgar10QParser().parse(html)
 
-print(elements[0])
+# Let's print the first 5 elements:
+for element in elements[:5]:
+    element_type = element.__class__.__name__
+    element_text = element.text
+    print(f"Element Type: {element_type}")
+    print(f"Text Content: {element_text}")
+    print("-" * 40)  # Separator for readability
